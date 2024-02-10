a9d11829b904e8775c26619450b3525d03f85311
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-02-10 18:01:59 -0500

    Add slim slider manually

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
     $('.slider').slick({
         dots: false, // Disable dots navigation
         infinite: true,
+        autoplay: true,
         speed: 500,
         slidesToShow: 1, // Show one project per slide
         slidesToScroll: 1,
-        prevArrow: '<button type="button" class="slick-prev">Previous</button>', // Custom previous arrow
-        nextArrow: '<button type="button" class="slick-next">Next</button>', // Custom next arrow
+        prevArrow: '<button type="button" id="prev-arrow" class="slick-prev">Previous</button>', // Custom previous arrow
+        nextArrow: '<button type="button" id="next-arrow" class="slick-next">Next</button>', // Custom next arrow
     });
 });
