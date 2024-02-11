4a82a896dca8e2001c9df175242ec20aabed4af5
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-02-11 10:37:24 -0500

    Change structure of slides

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         infinite: true,
         autoplay: true,
         speed: 500,
-        slidesToShow: 1, // Show one project per slide
+        slidesToShow: 2, // Show one project per slide
         slidesToScroll: 1,
         prevArrow: '<button type="button" id="prev-arrow" class="slick-prev">Previous</button>', // Custom previous arrow
         nextArrow: '<button type="button" id="next-arrow" class="slick-next">Next</button>', // Custom next arrow
